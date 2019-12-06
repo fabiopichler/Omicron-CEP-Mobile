0d1f3876218d44f8811ce81c84f098d84c46d190
Fabio Pichler <fabiopichler1992@gmail.com>
2019-12-06 05:48:13 -0300

    updated

--- src/app/screens/homeScreen/homeButton/HomeButton.tsx
+++ src/app/screens/homeScreen/homeButton/HomeButton.tsx
@@ -52,7 +52,7 @@ const HomeButton: React.FC<IHomeButtonProps> = ({
         >
             <>
                 <Text style={styles.textTop}>
-                    Pesquisa pelo
+                    Pesquisa por
                 </Text>
 
                 <View style={styles.textView}>
