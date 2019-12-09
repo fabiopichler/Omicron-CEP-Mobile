20092abb1687e256cec8333970ec6ad758880078
Fabio Pichler <fabiopichler1992@gmail.com>
2019-12-09 02:38:18 -0300

    update dark theme

--- src/themes/darkTheme.ts
+++ src/themes/darkTheme.ts
@@ -7,22 +7,22 @@ export const darkTheme: ITheme = {
     mode: 'exact',
     colors: {
         ...DarkTheme.colors,
-        primary: '#003020',
-        accent: '#003927',
-        surface: '#2a2a2a',
+        primary: '#044343',
+        accent: '#0f4e4e',
+        background: '#232931',
+        surface: '#393e46',
         text: 'white',
-        background: '#1e1e1e'
     },
     customColors: {
-        primaryDark: '#002a1a',
-        backgroundSecondary: '#262626',
-        title: '#2a7',
-        textPrimary: 'rgba(255,255,255,.8)',
+        primaryDark: '#003a37',
+        backgroundSecondary: '#232931',
+        title: '#4ecca3',
+        textPrimary: '#4ecca3',
         textSecondary: 'rgba(255,255,255,.8)',
     },
     customStyles: {
         card: {
-            borderRadius: 8,
+            borderRadius: 6,
             elevation: 0,
         }
     }
